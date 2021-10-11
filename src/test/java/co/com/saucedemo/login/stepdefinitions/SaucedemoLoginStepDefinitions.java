8f76bbe6d9ed1eaf600b60c817c77147062021c4
Leidy Moreno <leidymoreno17@gmail.com>
2021-10-10 20:06:03 -0500

    Proyectos SauceDemo

---
+++ src/test/java/co/com/saucedemo/login/stepdefinitions/SaucedemoLoginStepDefinitions.java
@@ -0,0 +1,25 @@
+package co.com.saucedemo.login.stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class SaucedemoLoginStepDefinitions {
+	
+	@Given("^that leidy want to login in saucedemo website$")
+	public void thatLeidyWantToLoginInSaucedemoWebsite() throws Exception {
+		
+	}
+
+
+	@When("^she login to the page$")
+	public void sheLoginToThePage() throws Exception {
+		
+	}
+
+	@Then("^she should see the url https://www\\.saucedemo\\.com/inventory\\.html in the screen$")
+	public void sheShouldSeeTheUrlHttpsWwwSaucedemoComInventoryHtmlInTheScreen() throws Exception {
+
+	}
+
+}
---
+++ src/test/resources/features/saucedemo_login.feature
@@ -0,0 +1,15 @@
+#Author: your.email@your.domain.com
+
+Feature: Login
+  As a web user
+  I want to log in to the suacedemo	website
+  to search products on the website
+
+
+  Scenario: login in to the saucedemo website
+    Given that leidy want to login in saucedemo website
+    When she login to the page
+    Then she should see the url https://www.saucedemo.com/inventory.html in the screen
+
+
+
